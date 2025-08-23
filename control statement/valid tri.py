96374a4b45b0859d582d8706dff09c3f5850275c
himanshigarg0202 <himanshi.g1604@gmail.com>
2025-08-23 16:47:59 +0530

    Update and rename validtri.py to valid tri.py

--- control statement/valid tri.py
+++ control statement/valid tri.py
@@ -4,6 +4,6 @@ b = int(input("Enter angle B: "))
 c = int(input("Enter angle C: "))
 
 if a > 0 and b > 0 and c > 0 and (a + b + c == 180):
-    print("The triangle is valid")
+    print("triangle is valid")
 else:
-    print("The triangle is NOT valid")
+    print("triangle is NOT valid")
