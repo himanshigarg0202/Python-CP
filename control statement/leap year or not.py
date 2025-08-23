a028448663fee6e42c72eed99dc34b4075c230a2
himanshigarg0202 <himanshi.g1604@gmail.com>
2025-08-23 16:45:56 +0530

    Update leap year or not.py

--- control statement/leap year or not.py
+++ control statement/leap year or not.py
@@ -3,6 +3,6 @@
 Y = int(input("Enter a year: "))
 
 if (Y % 4 == 0 and Y % 100 != 0) or (Y % 400 == 0):
-    print("It is a Leap year")
+    print("Leap year")
 else:
-    print("It is Not a leap year")
+    print("Not a leap year")
