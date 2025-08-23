52657705958cd7fa459fbcec8343ef0e0f3de463
himanshigarg0202 <himanshi.g1604@gmail.com>
2025-08-23 16:41:28 +0530

    Update and rename dispayday.py to dispay day.py

---
+++ control statement/dispay day.py
@@ -0,0 +1,20 @@
+#day and number
+daynumber = int(input("enter a number (1 to 7)"))
+
+if daynumber == 1:
+    print("Sunday")
+elif daynumber == 2:
+    print("Monday")
+elif daynumber == 3:
+    print("Tuesday")
+elif daynumber == 4:
+    print("Wednesday")
+elif daynumber == 5:
+    print("Thursday")
+elif daynumber == 6:
+    print("Friday")
+elif daynumber == 7:
+    print("Saturday")
+else:
+    print("enter a number 1 and 7")
+
