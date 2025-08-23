bebf62801230750c4b735c84e197ab9349d06da6
himanshigarg0202 <himanshi.g1604@gmail.com>
2025-08-23 16:38:17 +0530

    Update and rename 3intandmax.py to 3int and max.py

--- control statement/3int and max.py
+++ control statement/3int and max.py
@@ -1,8 +1,8 @@
 #3 integer and find max
 
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-c = int(input("Enter third number: "))
+a = int(input("Enter first number "))
+b = int(input("Enter second number "))
+c = int(input("Enter third number "))
 
 if a >= b and a >= c:
     maximum = a
