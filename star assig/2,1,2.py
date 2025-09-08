77dba0ba177654c389b1e5bfda94040f75efd43d
himanshigarg0202 <himanshi.g1604@gmail.com>
2025-09-08 19:41:54 +0530

    Add files via upload

---
+++ star assig/2,1,2.py
@@ -0,0 +1,3 @@
+n=(2,2,1,2,2)
+for count in n:
+    print("*" * count)
---
+++ star assig/left side star.py
@@ -0,0 +1,8 @@
+n=(5,2,2,2,5)
+for count in n:
+    print("*" * count)
+
+
+# n=(6,3,3,3,3,6)
+# for count in n:
+#     print("*" * count)
