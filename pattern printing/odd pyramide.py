d0a972272b82df690018d72112dca7941f368dda
himanshigarg0202 <himanshi.g1604@gmail.com>
2025-09-18 20:46:26 +0530

    Update odd pyramide.py

--- pattern printing/odd pyramide.py
+++ pattern printing/odd pyramide.py
@@ -1,6 +1,7 @@
-n=5
-for i in range(1, n + 1):
+n=4
+c=1
+for i in range(1,n+1):
     for j in range(i):
-        print(n, end=' ')
-        n += 1
+        print(c,end=" ")
+        c+=1
     print()
